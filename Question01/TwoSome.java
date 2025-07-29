2ab956bbd8b36208cb7a9d3c60b4b7ba25ab8e8b
devtushar001 <tc341269@gmail.com>
2025-07-29 16:06:33 +0530

    Two sum

---
+++ Question01/TwoSome.java
@@ -0,0 +1,37 @@
+package Question01;
+
+import java.util.HashMap;
+
+public class TwoSome {
+    public static int[] twoSome(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>(); // value -> index
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+
+        return new int[] {}; // if no match found
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 2, 7, 11, 15 };
+        int target = 9;
+
+        int[] result = twoSome(nums, target);
+
+        System.out.print("Output: [");
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i]);
+            if (i < result.length - 1)
+                System.out.print(", ");
+        }
+        System.out.println("]");
+    }
+}
+
+// hashmap,
