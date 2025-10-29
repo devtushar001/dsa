98825561b602043d813a7415c7349ce717981a13
devtushar001 <tc341269@gmail.com>
2025-10-29 17:20:05 +0530

    java learning for dsa

---
+++ DSA01/learning.java
@@ -0,0 +1,8 @@
+package DSA01;
+public class learning {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("Hello, DSA!");
+        System.out.println("Learning Java programming.");
+    }
+}
